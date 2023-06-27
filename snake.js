7c73140d747789770945325cd8abe3c94787d33f
nur aleina <nuraleina77@gmail.com>
2023-06-27 15:42:45 +0800

    add all

--- snake.js
+++ snake.js
@@ -41,6 +41,8 @@ class Snake{
 
 class Apple {
     constructor(){
+        console.log("apple")
+        console.log(snake.size)
         var isTouching;
         while(true){
             isTouching = false;
@@ -51,12 +53,15 @@ class Apple {
                     isTouching = true
                 }
             }
+            
+            console.log(this.x , this.y)
+            this.color = "red"
+            this.size = snake.size
+            console.log(this.x , this.y)
             if(!isTouching){
                 break;
             }
-            this.color = "yellow"
-            this.size = snake.size
-        }
+        } 
     }
 }
 
@@ -86,7 +91,30 @@ function update(){
     canvasContext,clearRect(0,0, canvas.width, canvas.height)
     console.log("update")
     snake.move()
+    eatApple()
+    checkHitWall();
 
+}
+
+function checkHitWall(){
+    var headTail = snake.tail[snake.tail.length - 1]
+    if (headTail.x == - snake.size) {
+        headTail.x = canvas.width - snake.size
+    } else if (headTail.x == canvas.widh) {
+        headTail.x = 0
+    } else if (headTail.y == - snake.size) {
+        headTail.y = canvas.height - snake.size
+    } else if (headTail.y == canvas.height) {
+        headTail.y = 0 
+    }
+}
+
+function eatApple(){
+    if(snake.tail[snake.tail.length - 1].x == apple.x &&
+      snake.tail[snake.tail.length - 1].y == apple.y){
+      snake.tail[snake.tail.length]= {x:apple.x, y:apple.y}
+      apple = new Apple ();
+      }
 
 }
 
@@ -100,8 +128,7 @@ function draw(){
 
     canvasContext.font ="20px Arial"
     canvasContext.fillStyle = "#00ff42"
-    canvasContext.fillText("Score:", (snake.tail.length +1),
-    canvas.width -120, 18);
+    canvasContext.fillText("Score:"+ (snake.tail.length -1),canvas.width -120, 18);
     createRect(apple.x, apple.y, apple.size, apple.size, apple.color )
 }
 
@@ -109,3 +136,22 @@ function createRect(x,y,width,height,color){
     canvasContext.fillStyle=color
     canvasContext.fillRect(x,y,width,height)
 }
+
+
+window.addEventListener("keydown", (event) => {
+    setTimeout(() => {
+        if (event.keyCode == 37 && snake.rotateX != 1) {
+            snake.rotateX = -1
+            snake.rotateY = 0
+        } else if (event.keyCode == 38 && snake.rotateY != 1) {
+            snake.rotateX = 0
+            snake.rotateY = -1
+        } else if (event.keyCode == 39 && snake.rotateX != -1) {
+            snake.rotateX = 1
+            snake.rotateY = 0
+        } else if (event.keyCode == 40 && snake.rotateY != -1) {
+            snake.rotateX = 0
+            snake.rotateY = 1
+        }
+    }, 1)
+})
